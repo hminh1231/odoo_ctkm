ec8053fc1338b39a3d71c2881e5f5f80e2b15ea7
Song Khoi <songkhoi123@gmail.com>
2026-08-19 15:00:22 +0700

    add preview design assests

--- custom_addons/ctkm_core/models/ctkm_task_tem_design_line.py
+++ custom_addons/ctkm_core/models/ctkm_task_tem_design_line.py
@@ -36,3 +36,30 @@ class CtkmTaskTemDesignLine(models.Model):
         help='Tải lên mẫu thiết kế (PDF hoặc ảnh): .pdf, .png, .jpg, .jpeg, .gif, .webp.',
     )
     filename = fields.Char(string='Tên file')
+
+    def _ctkm_file_url(self):
+        """URL xem file inline trên trình duyệt (PDF / ảnh) thay vì tải xuống."""
+        self.ensure_one()
+        if not self.file:
+            return False
+        return '/web/content?model=%s&id=%s&field=file&filename=%s&download=0' % (
+            self._name, self.id, self.filename or 'file',
+        )
+
+    def action_preview_file(self):
+        """Mở file mẫu thiết kế xem trước trong trình duyệt (tab mới)."""
+        self.ensure_one()
+        if not self.file:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'message': 'Chưa có file để xem.',
+                    'type': 'warning',
+                },
+            }
+        return {
+            'type': 'ir.actions.act_url',
+            'url': self._ctkm_file_url(),
+            'target': 'new',
+        }
